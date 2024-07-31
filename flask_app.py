801312dd34236c474f0f57cd7cacfce742d4bac3
coldwar1942 <chakrit.jira@gmail.com>
2024-07-31 13:20:30 +0000

    first pyhthon code

---
+++ flask_app.py
@@ -0,0 +1,110 @@
+from flask import Flask, request, abort
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
+from linebot import LineBotApi
+
+
+
+
+
+from linebot.models import FlexSendMessage
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('odz7P1Pu4YPBKfC2UaRJGzhP671gKFSR7DWrCKkBLCZaMUL4vRs62JDF9sfliaulr3C18QMazzHCXAZPBofFrBjs3schUsCWY9LoIbz0AH3PmGYb0COtKTDDwfqtlgJJ7W3mCN4YnYRwr41BTq6sKgdB04t89/1O/w1cDnyilFU=')
+configuration = Configuration(access_token='odz7P1Pu4YPBKfC2UaRJGzhP671gKFSR7DWrCKkBLCZaMUL4vRs62JDF9sfliaulr3C18QMazzHCXAZPBofFrBjs3schUsCWY9LoIbz0AH3PmGYb0COtKTDDwfqtlgJJ7W3mCN4YnYRwr41BTq6sKgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('29e4dc7397d37e92d3a17cf5f459364b')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
+
+def push_flex_message(to, alt_text, flex_content):
+    flex_message = FlexSendMessage(
+        alt_text=alt_text,
+        contents=flex_content
+    )
+    line_bot_api.push_message(to, flex_message)
+
+flex_content = {
+    "type": "bubble",
+    "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+            {
+                "type": "text",
+                "text": "Hello, this is a Flex Message!",
+                "weight": "bold",
+                "size": "xl"
+            },
+            {
+                "type": "box",
+                "layout": "baseline",
+                "margin": "md",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "More details here",
+                        "size": "sm",
+                        "color": "#999999"
+                    }
+                ]
+            }
+        ]
+    }
+}
+
+@app.route("/send_flex_message", methods=['GET'])
+def send_flex_message():
+    user_id = 'U941269aa045b31f401009f8a369f2198'  # Replace with the actual user ID
+    alt_text = 'This is a Flex Message'
+    push_flex_message(user_id, alt_text, flex_content)
+    return 'Flex Message sent!'
+
+if __name__ == "__main__":
+    app.run(port=8080)
---
+++ main3.py
@@ -0,0 +1,66 @@
+import schedule
+import time as tm
+from datetime import time, timedelta, datetime
+import subprocess
+import flask_app
+import datetime, pytz
+from neo4j import GraphDatabase, basic_auth
+
+time_str = 0;
+tz = pytz.timezone('Asia/Bangkok')
+now1 = datetime.datetime.now(tz)
+time_compare = now1.strftime('%H')
+
+driver = GraphDatabase.driver(
+  "neo4j://172.30.81.223:7687",
+  auth=basic_auth("neo4j", "password"))
+
+def job():
+    global time_str
+    global time_compare
+    now1 = datetime.datetime.now(tz)
+    time_str = now1.strftime('%H')
+     
+    cypher_query = '''
+    MATCH (n:USER) return n.pushTime as time,n.userID as UID;
+    '''
+
+    Entity_corpus = []
+    with driver.session() as session:
+        results = session.run(cypher_query)
+        for record in results:
+            #print(f"Time: {record['time']}, UID: {record['UID']}")
+            Entity_corpus.append((record['time'],record['UID']))
+        #using set() to remove duplicated from list
+        Entity_corpus = list(set(Entity_corpus))
+
+    for entity in Entity_corpus:
+        #print(f"Time: {entity[0]}, UID: {entity[1]}")
+
+        hour = entity[0][:2]
+        uid = entity[1]
+        print(hour)
+        print(uid)
+        if (time_str != time_compare and time_str == hour):
+            print("Subscribe to World")
+            run_flask_app();
+
+        time_compare = time_str
+    
+
+schedule.every(5).seconds.do(job)
+
+def run_flask_app():
+    #subprocess.run(["python", "flask_app.py"])
+    with flask_app.app.test_client() as client:
+        response = client.get('/send_flex_message')
+        print(response.data.decode())
+
+if __name__ == "__main__":
+    
+    
+    
+    while True:
+        schedule.run_pending()
+        tm.sleep(1)
+    
