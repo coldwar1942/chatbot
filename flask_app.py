693df115406dc335df600fb616935a9039a805d5
coldwar1942 <chakrit.jira@gmail.com>
2024-07-31 15:07:53 +0000

    webhook reply message basic

--- flask_app.py
+++ flask_app.py
@@ -21,10 +21,10 @@ from linebot.v3.webhooks import (
 from linebot import LineBotApi
 
 
+import json
+from flask import abort
 
-
-
-from linebot.models import FlexSendMessage
+from linebot.models import *
 
 app = Flask(__name__)
 
@@ -32,36 +32,35 @@ line_bot_api = LineBotApi('odz7P1Pu4YPBKfC2UaRJGzhP671gKFSR7DWrCKkBLCZaMUL4vRs62
 configuration = Configuration(access_token='odz7P1Pu4YPBKfC2UaRJGzhP671gKFSR7DWrCKkBLCZaMUL4vRs62JDF9sfliaulr3C18QMazzHCXAZPBofFrBjs3schUsCWY9LoIbz0AH3PmGYb0COtKTDDwfqtlgJJ7W3mCN4YnYRwr41BTq6sKgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('29e4dc7397d37e92d3a17cf5f459364b')
 
-
-@app.route("/callback", methods=['POST'])
-def callback():
+#@app.route("/callback", methods=['POST'])
+#def callback():
     # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+    #signature = request.headers['X-Line-Signature']
 
     # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    #body = request.get_data(as_text=True)
+    #app.logger.info("Request body: " + body)
 
     # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+    #try:
+        #handler.handle(body, signature)
+    #except InvalidSignatureError:
+        #app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        #abort(400)
 
-    return 'OK'
+    #return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessageContent)
-def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
-            )
-        )
+#@handler.add(MessageEvent, message=TextMessageContent)
+#def handle_message(event):
+    #with ApiClient(configuration) as api_client:
+        #line_bot_api = MessagingApi(api_client)
+        #line_bot_api.reply_message_with_http_info(
+           # ReplyMessageRequest(
+             #   reply_token=event.reply_token,
+              #  messages=[TextMessage(text=event.message.text)]
+            #)
+       # )
 
 def push_flex_message(to, alt_text, flex_content):
     flex_message = FlexSendMessage(
@@ -105,6 +104,58 @@ def send_flex_message():
     alt_text = 'This is a Flex Message'
     push_flex_message(user_id, alt_text, flex_content)
     return 'Flex Message sent!'
+
+@app.route("/", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)
+    try:
+        json_data = json.loads(body)
+        access_token = 'odz7P1Pu4YPBKfC2UaRJGzhP671gKFSR7DWrCKkBLCZaMUL4vRs62JDF9sfliaulr3C18QMazzHCXAZPBofFrBjs3schUsCWY9LoIbz0AH3PmGYb0COtKTDDwfqtlgJJ7W3mCN4YnYRwr41BTq6sKgdB04t89/1O/w1cDnyilFU='
+        secret = '29e4dc7397d37e92d3a17cf5f459364b'
+        line_bot_api = LineBotApi(access_token)
+        handler = WebhookHandler(secret)
+        signature = request.headers['X-Line-Signature']
+        handler.handle(body, signature)
+        msg = json_data['events'][0]['message']['text']
+        tk = json_data['events'][0]['replyToken']
+        user_id = json_data['events'][0]['source']['userId']
+        #msg_reply = reply_msg(msg,user_id)
+        #line_bot_api.reply_message(tk,TextSendMessage(text=msg_reply[0],quick_reply=msg_reply[1]))                             
+        reply_msg(line_bot_api,tk,user_id,msg)
+        print(msg, tk)
+        print("user_id",user_id)
+    except Exception as e:
+        print(e) 
+    return 'OK'
+
+
+def reply_msg(line_bot_api,tk,user_id,msg):
+    #result = check_user_id(msg,user_id,line_bot_api,tk)
+    #check_user_id(line_bot_api,tk,user_id,msg)
+    #text_msg = result[0]
+    response = TextSendMessage(text=f"Hellom {msg}")
+    line_bot_api.reply_message(tk,response)
+    #quick_reply_dic = result[1]
+    #text_result2 = result[2]
+   # if (result[2]==2):
+     #   text_msg = text_msg
+    #    return text_msg,quick_reply_dic
+   # else:
+      #  return result,""
+
+
+def check_user_id(line_bot_api,tk,user_id,msg):
+    if (msg == "Hello"):
+        return_message(line_bot_api,tk,user_id,msg)
+
+
+def return_message(line_bot_api,tk,user_id,msg):
+    if(msg == "Hello"):
+        flex = response_flex()
+        line_bot_api.reply_message(tk,fkex)
+
+
+
 
 if __name__ == "__main__":
     app.run(port=8080)
